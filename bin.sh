e14166db1051bf2f2b08823de497c2fbd3f9a9d9
oijm17 <oijm17@users.noreply.github.com>
2017-06-12 02:28:57 -0500

    Create bin.sh

--- bin.sh
+++ bin.sh
@@ -11,7 +11,7 @@ install_ffmpeg() {
     cd FFmpeg-master/
     ./configure --prefix=/usr --enable-shared --enable-nonfree \
     --enable-gpl --enable-pthreads --enable-decoder=liba52 \
-    --enable-libfdk-aac --enable-libmp3lame \
+    --enable-libfdk-aac --enable-libopus --enable-libmp3lame \
     --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid \
     --extra-cflags=-I/usr/include/ --extra-ldflags=-L/usr/lib \
     --enable-version3 --extra-version=syslint --enable-libopencore-amrnb \

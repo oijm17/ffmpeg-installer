97fa7bcd3af8ef26f80eb7858973f9f32dbbce54
oijm17 <oijm17@users.noreply.github.com>
2017-06-26 20:33:55 -0500

    Update req.sh

--- req.sh
+++ req.sh
@@ -95,7 +95,7 @@ run_yum() {
     yum groupinstall "Development Tools" -y
     yum install ruby php-devel boost-devel curl-devel gcc gcc-c++ libgcc gd gd-devel gettext \
     cmake mercurial libjpeg* libjpeg-devel* libstdc++* libstdc++-devel* libtiff* \
-    libtiff-devel* opus-devel* libtool* libxml* libxml2* wget \
+    libtiff-devel* opus-devel* libtool* libxml* libxml2* wget ant \
     libxml2-devel* zlib* zlib-devel* automake* autoconf* samba-common* \
     ncurses-devel ncurses patch make apr-util giflib-devel giflib neon \
     expat-devel gettext-devel openssl-devel subversion SDL-devel freeglut-devel perl -y
